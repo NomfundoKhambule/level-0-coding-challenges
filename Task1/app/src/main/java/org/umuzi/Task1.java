91c4ef35f098be0c12dab3760dbfdf546df697bb
Nomfundo Khambule <nomfundo.khambule02@gmail.com>
2022-01-18 22:58:19 +0200

    added task 0.1

---
+++ Task1/app/src/main/java/org/umuzi/Task1.java
@@ -0,0 +1,17 @@
+package org.umuzi;
+
+public class Task1 
+{
+  public static void main(String[] args) 
+  {
+    int x =0;
+    int y =1;
+    System.out.println(x);
+    System.out.println(y);
+    x += 3;
+    y += x;
+    System.out.println(x);
+    System.out.println(y);
+   
+  }
+}
