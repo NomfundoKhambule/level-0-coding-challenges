9fef41d0f78e432947085ea56f666b5da732f754
Nomfundo Khambule <nomfundo.khambule02@gmail.com>
2022-01-18 23:21:59 +0200

    added task 0.4

---
+++ Task4/app/src/main/java/org/umuzi/Task4.java
@@ -0,0 +1,21 @@
+package org.umuzi;
+
+public class Task4 
+{ static void evenOrOdd(int x)
+  {
+    if(x%2 == 0)
+    {
+      System.out.println("even");
+    }
+    else
+    {
+      System.out.println("odd");
+    }
+  }
+
+
+  public static void main(String[] args) 
+  {
+    evenOrOdd(100);
+  }
+}
