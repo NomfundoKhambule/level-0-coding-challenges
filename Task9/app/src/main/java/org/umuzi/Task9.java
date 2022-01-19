7650986cfec81d49d2f6299ea98295addd15fe81
Nomfundo Khambule <nomfundo.khambule02@gmail.com>
2022-01-19 13:03:58 +0200

    added task 0.9 changed

--- Task9/app/src/main/java/org/umuzi/Task9.java
+++ Task9/app/src/main/java/org/umuzi/Task9.java
@@ -8,6 +8,7 @@ public class Task9
     String newString = word.toLowerCase();
     char[] charArray= newString.toCharArray();
     Set<Character> vowels = new HashSet<Character>();
+    System.out.print("Vowels: ");
     for(int i=0;i<charArray.length;i++)
     {
         if(charArray[i]=='a'||charArray[i]=='e'||charArray[i]=='i'||charArray[i]=='o'||charArray[i]=='u')
@@ -16,7 +17,20 @@ public class Task9
         }
       
     }
-    System.out.println(vowels);
+    Character[] vowelsArray = new Character[vowels.size()];
+    vowelsArray = vowels.toArray(vowelsArray);
+    for(int i = 0; i < vowelsArray.length;i++)
+    {
+      if(i != vowelsArray.length -1)
+      {
+        System.out.print(vowelsArray[i]+", ");
+      }
+      else
+      {
+        System.out.print(vowelsArray[i]);
+      }
+    }
+    
   }
 
   public static void main(String[] args) 
