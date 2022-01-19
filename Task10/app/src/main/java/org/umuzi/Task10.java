b6b4865c6fbc072f7dbc833ab147ff9dca29b281
Nomfundo Khambule <nomfundo.khambule02@gmail.com>
2022-01-19 13:04:31 +0200

    added task o.10 changes

--- Task10/app/src/main/java/org/umuzi/Task10.java
+++ Task10/app/src/main/java/org/umuzi/Task10.java
@@ -5,23 +5,36 @@ public class Task10
 {
   static void commonLetters(String word1, String word2)
   {
-    Set<Character> sameLetters = new HashSet<Character>();
+    Set<Character> same = new HashSet<Character>();
     char[] charArray1 = word1.toLowerCase().toCharArray();
     char[] charArray2 = word2.toLowerCase().toCharArray();
+    System.out.print("Common letters: ");
     for(int i = 0; i<charArray1.length;i++)
     {
       for(int j=0; j<charArray2.length;j++)
       {
         if(charArray1[i]==charArray2[j])
         {
-          sameLetters.add(charArray2[j]);
+          same.add(charArray2[j]);
         }
       }
       
     }
-    System.out.println(sameLetters);
+    Character[] sameLettersArray = same.toArray(new Character[same.size()]);
+    for(int i = 0; i < sameLettersArray.length;i++)
+    {
+      if(i != sameLettersArray.length -1)
+      {
+        System.out.print(sameLettersArray[i]+", ");
+      }
+      else
+      {
+        System.out.print(sameLettersArray[i]);
+      }
+    }
+    
   }
-
+  
   public static void main(String[] args) 
   {
     commonLetters("house","computers");
